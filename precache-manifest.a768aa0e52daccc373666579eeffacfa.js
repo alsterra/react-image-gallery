1bbe18fea34e65710072fbf2efd67d0d16e1f824
filayaty <imfilayaty@gmail.com>
2020-06-29 11:48:37 +0700

    Updates

---
+++ precache-manifest.a768aa0e52daccc373666579eeffacfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58073b56107384147ecdca549943226b",
+    "url": "/react-image-gallery/index.html"
+  },
+  {
+    "revision": "cbb0bdf529b052ada2fb",
+    "url": "/react-image-gallery/static/css/main.cda36a3e.chunk.css"
+  },
+  {
+    "revision": "d0503257279ceb636b83",
+    "url": "/react-image-gallery/static/js/2.c26e3a26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-image-gallery/static/js/2.c26e3a26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbb0bdf529b052ada2fb",
+    "url": "/react-image-gallery/static/js/main.9ff7f755.chunk.js"
+  },
+  {
+    "revision": "11ad39eeb649f02fa41d",
+    "url": "/react-image-gallery/static/js/runtime-main.e39fd300.js"
+  }
+]);
